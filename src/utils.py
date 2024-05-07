56c9ad9da4aa3285c5904b2fe3108da32a6a033b
Swati-Sharon <swatihansda05@gmail.com>
2024-05-07 13:40:56 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -39,9 +39,6 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             #model training
             model.fit(X_train,y_train)
 
-
-            
-
             y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
